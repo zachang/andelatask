965aa06f624dec590d26743385faab6f3b5f06a3
zachang <zachangdawuda@gmail.com>
2017-07-31 21:56:37 +0100

    Completed test for aritGeo task

--- aritGeo.js
+++ aritGeo.js
@@ -3,7 +3,9 @@
 class AritGeo{
 
     Arit(array){
-      if (array.length == 0) return "0";
+
+      if (!(array instanceof Array)) return "Not an array";
+      if (array.length == 0) return 0;
       if (array.length <= 2 && array.length > 0) return "Enter atleast 3 array index values";
 
         let arr_aritGeo = -1;
@@ -12,6 +14,7 @@ class AritGeo{
         let i;
 
         for (i = 2; i < array.length; i++) {
+
                if ((a += d) == array[i]){
                    arr_aritGeo = "Arithmetic";
                }
@@ -24,6 +27,7 @@ class AritGeo{
     }
 
     Geop(array){
+      if (!(array instanceof Array)) return "Not an array";
       if (array.length == 0) return 0;
       if (array.length <= 2 && array.length > 0) return "Enter atleast 3 array index values";
 
@@ -46,45 +50,5 @@ class AritGeo{
 }
 
 const aritGeo = new AritGeo();
+// console.log(aritGeo.Geop("aaa"));
 module.exports = aritGeo;
-//console.log(typeof array);
-
-// module.exports.aritGeo = (array) => {
-//     if (array.length == 0) return "Array empty, Enter atleast 3 values";
-//     if (array.length <= 2 && array.length > 0) return "Enter atleast 3 array index values";
-//
-//
-//     let arr_aritGeo = -1;
-//     let a = array[1];
-//     let r = a/array[0];
-//     let i;
-//
-//     for (i = 2; i < array.length; i++) {
-//         if ((a *= r) == array[i]){
-//             arr_aritGeo = "Geometric";
-//         }
-//         else{
-//             arr_aritGeo;
-//             break;
-//         }
-//     };
-//
-//     if (arr_aritGeo == "Geometric") return arr_aritGeo;
-//
-//     a = array[1];
-//     let d = a - array[0];
-//     i;
-//
-//     for (i = 2; i < array.length; i++) {
-//         if ((a += d) == array[i]){
-//             arr_aritGeo = "Arithmetic";
-//         }
-//         else {
-//             arr_aritGeo;
-//             break;
-//         }
-//     };
-//     return arr_aritGeo;
-// }
-
-// console.log(module.exports.aritGeo([1,2,3]));
--- aritGeo.test.js
+++ aritGeo.test.js
@@ -1,13 +1,59 @@
+const {assert} = require("chai");
 const aritGeotest = require("./aritGeo");
-describe(" Geometric progression:", () => {
 
-  it("should test if array is emtpy and throw an error", () =>{
-    let res = aritGeotest.Arit([]);
-    console.log(res);
-    if(res == 0 && res !== "Arithmetic"){
-      throw new Error(`${res}`);
-    }
+describe(" Arithmetic progression:", () => {
 
+  it("should return 0 if array is emtpy", () =>{
+      let res = aritGeotest.Arit([]);
+      assert.equal(res, 0, `Not Empty Array`);
   });
 
+  it("should return: Enter atleast 3 array index values, if array.length < 2", () =>{
+      let res = aritGeotest.Arit([2,3]);
+      assert.equal(res, "Enter atleast 3 array index values", `Array length > 2`);
+  });
+
+  it("should return -1 if not Arithmetic", () =>{
+      let res = aritGeotest.Arit([2,4,5]);
+      assert.equal(res, -1, `Arithmetic`);
+  });
+
+  it("should return Arithmetic", () =>{
+      let res = aritGeotest.Arit([2,4,6]);
+      assert.equal(res, 'Arithmetic', 'Not Arithmetic');
+  });
+
+  it("should return 'Not an array'", () =>{
+      let res = aritGeotest.Arit("aaa");
+      assert.equal(res, "Not an array");
+  });
+
+});
+
+describe("Geometric progression:", () => {
+
+  it("should return 0 if array is emtpy", () =>{
+      let res = aritGeotest.Geop([]);
+      assert.equal(res, 0, `Not Empty Array`);
+  });
+
+  it("should return: Enter atleast 3 array index values, if array.length < 2", () =>{
+      let res = aritGeotest.Geop([2,3]);
+      assert.equal(res, "Enter atleast 3 array index values", `Array length > 2`);
+  });
+
+  it("should return -1 if not Geometric", () =>{
+      let res = aritGeotest.Geop([3,6,11]);
+      assert.equal(res, -1, `Geometric`);
+  });
+
+  it("should return Geometric", () =>{
+      let res = aritGeotest.Geop([2,4,8]);
+      assert.equal(res, 'Geometric', 'Not Geometric');
+  });
+
+  it("should return 'Not an array'", () =>{
+      let res = aritGeotest.Geop(1,1);
+      assert.equal(res, "Not an array");
+  });
 });
